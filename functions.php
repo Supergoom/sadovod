618ea676eddb142da0be433bd0d5b4ad13c2bd64
Dmitrii Klimenko <777soten@mail.ru>
2023-09-29 01:32:25 +0300

    последняя версия данных с хостинга на 29.09.2023 01:32

--- functions.php
+++ functions.php
@@ -62,6 +62,34 @@ if (wp_doing_ajax()) {
     include_once('requests/filter-blog.php');
 };
 
+/*   Дополнительные поля пользователя ------------------------------*/
+include_once('settings/fields/user_fields.php');
+include_once('settings/fields/admin_fields.php');
+
+
+/*   Регистрация пользователя ------------------------------*/
+include_once('registration/user.php');
+
+/*   Получаем данные с кастомных полей ------------------------------*/
+include_once('layouts/widgets/custom_fields.php');
+
+function admin_js()
+{
+    ?><script>
+		jQuery(document).ready(function ($) {
+			$('tr:not(.acf-clone) .acf-table [data-key="field_6515696bdbf81"]').each(function (index) {
+				let val = $(this).find('.acf-accordion-content [data-name="name_municipality"] .acf-input input').val();
+				if (val) {
+					$(this).find('.acf-accordion-title label').html(val);
+				}
+			});
+		});
+    </script><?php
+}
+
+add_action('admin_head', 'admin_js');
+
+
 // Перенаправление со страниц входа
 
 //add_action( 'init', 'level_check' );
@@ -103,203 +131,3 @@ function register_link_url($url)
 }
 
 add_filter('register', 'register_link_url', 10, 2);
-
-// Функция транслитерации
-function transliterate($string) {
-    $converter = array(
-        'а' => 'a',   'б' => 'b',   'в' => 'v',
-        'г' => 'g',   'д' => 'd',   'е' => 'e',
-        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
-        'и' => 'i',   'й' => 'i',   'к' => 'k',
-        'л' => 'l',   'м' => 'm',   'н' => 'n',
-        'о' => 'o',   'п' => 'p',   'р' => 'r',
-        'с' => 's',   'т' => 't',   'у' => 'u',
-        'ф' => 'f',   'х' => 'kh',   'ц' => 'tc',
-        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'shch',
-        'ь' => '',  'ы' => 'y',   'ъ' => '',
-        'э' => 'e',   'ю' => 'iu',  'я' => 'ia',
-        '’' => ' ',  '.' => '',
-
-        'А' => 'A',   'Б' => 'B',   'В' => 'V',
-        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
-        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
-        'И' => 'I',   'Й' => 'I',   'К' => 'K',
-        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
-        'О' => 'O',   'П' => 'P',   'Р' => 'R',
-        'С' => 'S',   'Т' => 'T',   'У' => 'U',
-        'Ф' => 'F',   'Х' => 'Kh',   'Ц' => 'Tc',
-        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Shch',
-        'Ь' => '',  'Ы' => 'Y',   'Ъ' => '',
-        'Э' => 'E',   'Ю' => 'Iu',  'Я' => 'Ia',
-    );
-    return strtr($string, $converter);
-}
-
-
-// Дополнительные поля пользователя
-
-// когда пользователь сам редактирует свой профиль
-add_action( 'show_user_profile', 'true_show_profile_fields' );
-
-// когда чей-то профиль редактируется админом
-add_action( 'edit_user_profile', 'true_show_profile_fields' );
-
-function true_show_profile_fields( $user ) {
-
-    echo '<h1 style="color:red">Дополнительные данные пользователя</h1>';
-    echo '<table class="form-table" role="presentation">';
-
-    // добавляем поле Фамилия
-    $last_name = get_the_author_meta( 'last_name', $user->ID );
-    echo '<tr><th><label for="last_name">Фамилия</label></th>
- 	<td><input type="text" name="last_name" id="last_name" value="' . esc_attr($last_name) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Имя
-    $first_name = get_the_author_meta( 'first_name', $user->ID );
-    echo '<tr><th><label for="first_name">Имя</label></th>
- 	<td><input type="text" name="first_name" id="first_name" value="' . esc_attr($first_name) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Отчество
-    $patronymic = get_the_author_meta( 'patronymic', $user->ID );
-    echo '<tr><th><label for="patronymic">Отчество</label></th>
- 	<td><input type="text" name="patronymic" id="patronymic" value="' . esc_attr($patronymic) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Телефон
-    $tel = get_the_author_meta( 'tel', $user->ID );
-    echo '<tr><th><label for="tel">Телефон</label></th>
- 	<td><input type="text" name="tel" id="tel" value="' . esc_attr($tel) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Email
-    $email = get_the_author_meta( 'email', $user->ID );
-    echo '<tr><th><label for="email">Email</label></th>
- 	<td><input type="text" name="email" id="email" value="' . esc_attr($email) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Название СНТ
-    $namesnt = get_the_author_meta( 'namesnt', $user->ID );
-    echo '<tr><th><label for="namesnt">Название СНТ</label></th>
- 	<td><input type="text" name="namesnt" id="snt_name" value="' . esc_attr($namesnt) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Квдвстровый номер
-    $cadastral_num = get_the_author_meta( 'cadastral_num', $user->ID );
-    echo '<tr><th><label for="cadastral_num">Кадастровый номер</label></th>
- 	<td><input type="text" name="cadastral_num" id="cadastral_num" value="' . esc_attr($cadastral_num) . '" class="regular-text" /></td>
-	</tr>';
-    // добавляем поле Адрес
-    $address = get_the_author_meta( 'address', $user->ID );
-    echo '<tr><th><label for="address">Адрес</label></th>
- 	<td><input type="text" name="address" id="address" value="' . esc_attr($address) . '" class="regular-text" /></td>
-	</tr>';
-
-    echo '</table>';
-
-}
-
-// когда пользователь сам редактирует свой профиль
-add_action( 'personal_options_update', 'true_save_profile_fields' );
-// когда чей-то профиль редактируется админом например
-add_action( 'edit_user_profile_update', 'true_save_profile_fields' );
-
-function true_save_profile_fields( $user_id ) {
-
-    update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST[ 'last_name' ] ) );
-    update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST[ 'first_name' ] ) );
-    update_user_meta( $user_id, 'patronymic', sanitize_text_field( $_POST[ 'patronymic' ] ) );
-    update_user_meta( $user_id, 'tel', sanitize_text_field( $_POST[ 'tel' ] ) );
-    update_user_meta( $user_id, 'email', sanitize_text_field( $_POST[ 'email' ] ) );
-    update_user_meta( $user_id, 'namesnt', sanitize_text_field( $_POST[ 'namesnt' ] ) );
-    update_user_meta( $user_id, 'cadastral_num', sanitize_text_field( $_POST[ 'cadastral_num' ] ) );
-    update_user_meta( $user_id, 'address', sanitize_text_field( $_POST[ 'address' ] ) );
-}
-
-
-
-// Contact Form 7 регистрация пользователя
-
-function create_user_from_registration($cfdata) {
-    if (!isset($cfdata->posted_data) && class_exists('WPCF7_Submission')) {
-
-        $submission = WPCF7_Submission::get_instance();
-        if ($submission) {
-            $formdata = $submission->get_posted_data();
-        }
-    } elseif (isset($cfdata->posted_data)) {
-        $formdata = $cfdata->posted_data;
-    } else {
-        return $cfdata;
-    }
-
-    // Check this is the user registration form
-    if ( $cfdata->title() == 'Запрос на регистрацию собственника') {
-        //$pass = wp_generate_password( 12, false );
-
-        $last_name = sanitize_text_field($formdata['last_name']);
-        $name = sanitize_text_field($formdata['first_name']);
-        $patronymic = sanitize_text_field($formdata['patronymic']);
-        $tel = sanitize_text_field($formdata['tel']);
-        $email = sanitize_text_field($formdata['email']);
-        $namesnt = sanitize_text_field($formdata['namesnt']);
-        $cadastral_num = sanitize_text_field($formdata['cadastral_num']);
-        $address = sanitize_text_field($formdata['address']);
-        $pass = sanitize_text_field($formdata['pass']);
-
-        // Construct a username from the user's name
-        $username = strtolower(str_replace(' ', '', transliterate($name)));
-        $name_parts = explode(' ', $name);
-        if ( !email_exists( $email ) ) {
-            // Find an unused username
-            $username_tocheck = $username;
-            $i = 1;
-            while ( username_exists( $username_tocheck ) ) {
-                $username_tocheck = $username . $i++;
-            }
-            $username = $username_tocheck;
-            // Create the user
-            $userdata = [
-                'user_login' => $username,
-                'user_pass' => $pass,
-                'user_email' => $email,
-                'nickname' => reset($name_parts),
-                'display_name' => $name,
-                'first_name' => $name,
-                'last_name' => $last_name,
-                'role' => 'subscriber'
-            ];
-            $userMetaData = [
-                'patronymic' => $patronymic,
-                'tel' => $tel,
-                'namesnt'  => $namesnt,
-                'cadastral_num' => $cadastral_num,
-                'address' => $address,
-            ];
-
-            $user_id = wp_insert_user( $userdata );
-
-            if ( !is_wp_error($user_id) ) {
-
-                if ( isset($userMetaData['patronymic']) )
-                    update_user_meta($user_id, 'patronymic', $userMetaData['patronymic']);
-                if ( isset($userMetaData['tel']) )
-                    update_user_meta($user_id, 'tel', $userMetaData['tel']);
-                if ( isset($userMetaData['namesnt']) )
-                    update_user_meta($user_id, 'namesnt', $userMetaData['namesnt']);
-                if ( isset($userMetaData['cadastral_num']) )
-                    update_user_meta($user_id, 'cadastral_num', $userMetaData['cadastral_num']);
-                if ( isset($userMetaData['address']) )
-                    update_user_meta($user_id, 'address', $userMetaData['address']);
-
-
-                $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
-
-                $message = "Добро пожаловать! Ваши данные для входа :" . "\r\n</br>";
-                $message .= sprintf(__('Имя пользователя: %s'), $username) . "\r\n</br>";
-                $message .= sprintf(__('Пароль : %s'), $pass) . "\r\n</br>";
-                $message .=  "<a href='https://gissnt.ru/login/'>Перейти в личный кабинет</a>\r\n</br>";
-                wp_mail($email, sprintf(__('[%s] Ваше имя пользователя и пароль'), $blogname), $message);
-            }
-        }
-    }
-    return $cfdata;
-}
-add_action('wpcf7_before_send_mail', 'create_user_from_registration', 1);
-
-
--- layouts/templates/user/account.php
+++ layouts/templates/user/account.php
@@ -8,58 +8,122 @@ get_header();
 global $user_ID;
 
 // если пользователь не авторизован, отправляем его на страницу входа
-if( !$user_ID ) {
+if (!$user_ID) {
     header('location:' . site_url() . '/login');
     exit;
 } else {
-    $userdata = get_user_by( 'id', $user_ID );
+    $userdata = get_user_by('id', $user_ID);
 }
 
-if( isset($_GET['status']) ) :
-    switch( $_GET['status'] ) :
-        case 'ok':{
-            echo '<div class="success">Сохранено.</div>';
-            break;
-        }
-        case 'exist':{
-            echo '<div class="error">Пользователь с указанным email уже существует.</div>';
-            break;
-        }
-        case 'short':{
-            echo '<div class="error">Пароль слишком короткий.</div>';
-            break;
-        }
-        case 'mismatch':{
-            echo '<div class="error">Пароли не совпадают.</div>';
-            break;
-        }
-        case 'wrong':{
-            echo '<div class="error">Старый пароль неверен.</div>';
-            break;
-        }
-        case 'required':{
-            echo '<div class="error">Пожалуйста, заполните все обязательные поля.</div>';
-            break;
-        }
+if (isset($_GET['status'])) :
+    switch ($_GET['status']):
+        case 'ok': {
+                echo '<div class="success">Сохранено.</div>';
+                break;
+            }
+        case 'exist': {
+                echo '<div class="error">Пользователь с указанным email уже существует.</div>';
+                break;
+            }
+        case 'short': {
+                echo '<div class="error">Пароль слишком короткий.</div>';
+                break;
+            }
+        case 'mismatch': {
+                echo '<div class="error">Пароли не совпадают.</div>';
+                break;
+            }
+        case 'wrong': {
+                echo '<div class="error">Старый пароль неверен.</div>';
+                break;
+            }
+        case 'required': {
+                echo '<div class="error">Пожалуйста, заполните все обязательные поля.</div>';
+                break;
+            }
     endswitch;
 endif;
 
 // profile-update.php - обрабатывает сохранение
 ?>
-<form action="<?php echo get_stylesheet_directory_uri() ?>/profile-update.php" method="POST">
+
+<section class="account-card mt-[40px] mb-[80px]">
+    <div class="container mx-auto px-4">
+        <div class="section__title">Добро пожаловать в карточку садовода СНТ <?php echo get_user_meta($user_ID, 'namesnt', true) ?></div>
+        <div class="grid grid-cols-3 gap-[39px] mt-[40px] account-card__row">
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Ресурсы</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/resources.svg" alt="Ресурсы">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Реестр участков</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/registry.svg" alt="Реестр участков">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Документы СНТ</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/documents.svg" alt="Документы СНТ">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Общее собрание</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/people.svg" alt="Ресурсы">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Новости СНТ</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/news.svg" alt="Новости СНТ">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+            <div class="account-card__item">
+                <div class="account-card__top">
+                    <div class="account-card__title">Новости СНТ</div>
+                    <div class="account-card__icon">
+                        <img src="/wp-content/themes/sadovod/assets/img/icons/feedback.svg" alt="Новости СНТ">
+                    </div>
+                </div>
+                <a href="#" class="account-card__btn">Посмотреть</a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_template_part('layouts/templates/shared/section-message'); ?>
+
+<!-- <form action="<?php echo get_stylesheet_directory_uri() ?>/profile-update.php" method="POST">
 
     <input type="text" name="last_name" placeholder="Фамилия" value="<?php echo $userdata->last_name ?>" /><br>
     <input type="text" name="first_name" placeholder="Имя" value="<?php echo $userdata->first_name ?>" /><br>
-    <input type="text" name="patronymic" placeholder="Отчество" value="<?php echo get_user_meta($user_ID, 'patronymic', true ); ?>" /><br>
-    <input type="text" name="tel" placeholder="Телефон" value="<?php echo get_user_meta($user_ID, 'tel', true ) ?>" /><br>
+    <input type="text" name="patronymic" placeholder="Отчество" value="<?php echo get_user_meta($user_ID, 'patronymic', true); ?>" /><br>
+    <input type="text" name="tel" placeholder="Телефон" value="<?php echo get_user_meta($user_ID, 'tel', true) ?>" /><br>
     <input type="email" name="email" placeholder="Email" value="<?php echo $userdata->user_email ?>" /><br>
 
-    <?php if (get_user_meta($user_ID, 'namesnt', true )) ?>
-        <input type="text" name="namesnt" placeholder="Название СНТ" value="<?php echo get_user_meta($user_ID, 'namesnt', true ) ?>" /><br>
-    <?php if (get_user_meta($user_ID, 'cadastral_num', true )) ?>
-        <input type="text" name="cadastral_num" placeholder="Кадастровый номер" value="<?php echo get_user_meta($user_ID, 'cadastral_num', true ) ?>" /><br>
-    <?php if (get_user_meta($user_ID, 'address', true )) ?>
-        <input type="text" name="address" placeholder="Адрес" value="<?php echo get_user_meta($user_ID, 'address', true ) ?>" /><br>
+    <?php if (get_user_meta($user_ID, 'namesnt', true)) ?>
+    <input type="text" name="namesnt" placeholder="Название СНТ" value="<?php echo get_user_meta($user_ID, 'namesnt', true) ?>" /><br>
+    <?php if (get_user_meta($user_ID, 'cadastral_num', true)) ?>
+    <input type="text" name="cadastral_num" placeholder="Кадастровый номер" value="<?php echo get_user_meta($user_ID, 'cadastral_num', true) ?>" /><br>
+    <?php if (get_user_meta($user_ID, 'address', true)) ?>
+    <input type="text" name="address" placeholder="Адрес" value="<?php echo get_user_meta($user_ID, 'address', true) ?>" /><br>
 
 
     <input type="password" name="pwd1" placeholder="Старый пароль" /><br>
@@ -69,5 +133,5 @@ endif;
     <button>Сохранить</button>
 </form>
 
-    <br><br><a href="<?php echo wp_logout_url( home_url() ); ?>" title="Выход">Выход</a>
+<br><br><a href="<?php echo wp_logout_url(home_url()); ?>" title="Выход">Выход</a> -->
 <?php get_footer(); ?>
